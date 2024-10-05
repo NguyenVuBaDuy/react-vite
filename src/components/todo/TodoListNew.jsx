3778afe3facfec878cb8ac9c25ad683d6bb2ac57
Ba Duy <nguyenvubaduy9a11a@gmail.com>
2024-10-05 14:56:57 +0700

    video 36

--- src/components/todo/TodoListNew.jsx
+++ src/components/todo/TodoListNew.jsx
@@ -1,11 +1,21 @@
 
 const TodoListNew = (props) => {
     const { addNewToDo } = props;
-    addNewToDo('Ba Duy');
+
+    const handleOnChange = (name) => {
+        console.log('>>> handleOnChange : ' + name);
+    }
+
+    const handleOnClick = () => {
+        console.log('>>> handleOnclick run...');
+    }
+
     return (
         <div className='todo-new'>
-            <input type="text" placeholder='Enter your task' />
-            <button>Add</button>
+            <input type="text"
+                placeholder='Enter your task'
+                onChange={(event) => handleOnChange(event.target.value)} />
+            <button onClick={() => handleOnClick()}>Add</button>
         </div>
     )
 }
