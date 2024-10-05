f65e7e93a512dfabb2620154c0162f9321728857
Ba Duy <nguyenvubaduy9a11a@gmail.com>
2024-10-05 11:35:19 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,15 @@
+//JSX : only one parent
+//use "fragment" to fix if you have two parents
+//use className instead of class
+//if use style inline : use camel case instead of kebab case
+import './style.css';
+
 const MyComponent = () => {
     return (
-        <div>Nguyen Vu Ba Duy</div>
+        <>
+            <div>Nguyen Vu Ba Duy</div>
+            <div className="child" style={{ borderRadius: "10px" }}>child</div>
+        </>
     );
 }
 
