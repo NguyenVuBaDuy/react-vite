60c9dcd93801a418b29a1e1d5239d5088ac37d4e
Ba Duy <nguyenvubaduy9a11a@gmail.com>
2024-10-11 12:44:13 +0700

    video 81

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,5 +1,5 @@
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import { message, notification, Popconfirm, Table } from 'antd';
+import { notification, Popconfirm, Table } from 'antd';
 import UpdateUserModal from './update.user.modal';
 import { useState } from 'react';
 import ViewUserDetail from './view.user.detail';
