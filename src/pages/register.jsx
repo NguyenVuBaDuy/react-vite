30759da38aa880a6ea7f60216b788f56c70c0296
Ba Duy <nguyenvubaduy9a11a@gmail.com>
2024-10-14 12:39:41 +0700

    video 89

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,78 @@
+import { Button, Form, Input } from "antd";
+
 
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log(">>> Check values", values);
+    }
+
     return (
-        <div>register page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        >
+            <div style={{
+                margin: "50px",
+            }}>
+                <Form.Item
+                    label="Full name"
+                    name="fullName"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone number"
+                    name="phone"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+
+
+                <div>
+                    <Button type="primary" onClick={() => form.submit()}>Register</Button>
+                </div>
+            </div>
+        </Form >
     )
 }
 
