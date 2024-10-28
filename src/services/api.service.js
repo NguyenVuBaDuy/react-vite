d1aa82f205d59df426954e69230ba21ad7ebc9b7
Ba Duy <nguyenvubaduy9a11a@gmail.com>
2024-10-28 23:18:49 +0700

    video 102

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -72,6 +72,11 @@ const getAccountAPI = () => {
     return axios.get(URL_BACKEND)
 }
 
+const logoutAPI = () => {
+    const URL_BACKEND = "/api/v1/auth/logout"
+    return axios.post(URL_BACKEND)
+}
+
 export {
     createUserAPI,
     updateUserAPI,
@@ -81,5 +86,6 @@ export {
     updateUserAvatarAPI,
     registerUserAPI,
     loginAPI,
-    getAccountAPI
+    getAccountAPI,
+    logoutAPI
 }
